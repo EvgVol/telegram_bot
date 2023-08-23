833b0487e0e4f4f19d0c43085b1c3fbdf67dcdde
Evgeniy V <volochek93@yandex.ru>
2023-08-23 21:40:31 +0400

    added text-to-speech translation

--- bot.py
+++ bot.py
@@ -1,5 +1,12 @@
+import os
+
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters, ContextTypes
+from telegram.constants import ChatAction
+from telegram.ext import (ApplicationBuilder, CommandHandler,
+                          CallbackQueryHandler, MessageHandler,
+                          filters, ContextTypes)
+from gtts import gTTS
+from tempfile import NamedTemporaryFile
 
 from database import get_message_text_from_db
 from settings import TELEGRAM_TOKEN
@@ -7,27 +14,33 @@ from settings import TELEGRAM_TOKEN
 
 def get_main_menu_keyboard() -> InlineKeyboardMarkup:
     keyboard = [
-        [
-            InlineKeyboardButton('Фото из старшей школы', callback_data='school'),
-            InlineKeyboardButton('Последнее селфи', callback_data='selfie'),
-            InlineKeyboardButton('Пост про увлечения', callback_data='hobby'),
-            InlineKeyboardButton('Историю первой любви', callback_data='story'),
-            InlineKeyboardButton('Разницу между SQL и NoSQL', callback_data='difference'),
-            InlineKeyboardButton('Пришлю ссылку на резюме', callback_data='resume'),
-            InlineKeyboardButton('Что ещё умею', callback_data='more'),
-        ]
+        [InlineKeyboardButton('Фото из старшей школы',
+                              callback_data='school')],
+        [InlineKeyboardButton('Последнее селфи',
+                              callback_data='selfie')],
+        [InlineKeyboardButton('Пост про увлечения',
+                              callback_data='hobby')],
+        [InlineKeyboardButton('Историю первой любви',
+                              callback_data='story')],
+        [InlineKeyboardButton('Разницу между SQL и NoSQL',
+                              callback_data='difference')],
+        [InlineKeyboardButton('Пришлю ссылку на резюме',
+                              callback_data='resume')],
+        [InlineKeyboardButton('Что ещё умею',
+                              callback_data='more')],
     ]
     return InlineKeyboardMarkup(keyboard)
 
 
-async def handle_button_click(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def handle_button_click(update: Update,
+                              context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
     data_to_reply_function = {
         'selfie': query.message.reply_photo,
         'school': query.message.reply_photo,
     }
     message = get_message_text_from_db(query.data)
-    
+
     if query.data in data_to_reply_function:
         reply_function = data_to_reply_function[query.data]
         await reply_function(message)
@@ -39,7 +52,9 @@ async def handle_button_click(update: Update, context: ContextTypes.DEFAULT_TYPE
 
 def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     reply_markup = get_main_menu_keyboard()
-    message = get_message_text_from_db('start').format(name=update.effective_user.first_name)
+    message = get_message_text_from_db(
+        'start'
+    ).format(name=update.effective_user.first_name)
     return update.message.reply_text(message, reply_markup=reply_markup)
 
 
@@ -47,6 +62,21 @@ def what_can_you_do(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     reply_markup = get_main_menu_keyboard()
     message = get_message_text_from_db('what_can_you_do')
     return update.message.reply_text(message, reply_markup=reply_markup)  
+
+
+async def send_voice_message(update: Update,
+                             context: ContextTypes.DEFAULT_TYPE,
+                             text: str):
+    await context.bot.send_chat_action(chat_id=update.effective_chat.id,
+                                       action=ChatAction.RECORD_VOICE)
+    tts = gTTS(text, lang='ru')
+    with NamedTemporaryFile(suffix=".ogg", delete=False) as audio_file:
+        audio_file_name = audio_file.name
+        tts.save(audio_file_name)
+        audio_file.close()
+        with open(audio_file_name, 'rb') as f:
+            await update.message.reply_voice(f)
+        os.remove(audio_file_name)
 
 
 def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -63,7 +93,11 @@ def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         return action(update, context)
     else:
         message = get_message_text_from_db(action)
-        return update.message.reply_text(message)
+
+        if action == 'about_gpt':
+            return send_voice_message(update, context, message)
+        else:
+            return update.message.reply_text(message)
 
 
 def code(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -77,6 +111,7 @@ bot.add_handler(CommandHandler("start", start))
 bot.add_handler(CommandHandler("code", code))
 bot.add_handler(CommandHandler("menu", what_can_you_do))
 bot.add_handler(CallbackQueryHandler(handle_button_click))
-bot.add_handler(MessageHandler(filters.TEXT & ~ filters.COMMAND, text_handler))
+bot.add_handler(MessageHandler(filters.TEXT & ~ filters.COMMAND,
+                               text_handler))
 
 bot.run_polling()
